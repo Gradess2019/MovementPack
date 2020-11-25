bfe7aab073c42fb64d7b7e979519f1b4d91ec95b
Stepan <trofimovjava@gmail.com>
2020-11-25 21:49:47 +0300

    Add ADefaultGameMode and BP_DefaultGameMode

---
+++ Source/MovementPack/Public/GameModes/DefaultGameMode.h
@@ -0,0 +1,20 @@
+// Gradess & Novaturion Company. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DefaultGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVEMENTPACK_API ADefaultGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+
+public:
+	ADefaultGameMode(const FObjectInitializer& ObjectInitializer);
+};
